63cd85ab6cfcbd22ec7a790b9643fe129aed7ffc
Rohan Dhangar <rohandhangar1109@gmail.com>
2025-12-25 11:23:23 +0530

    started creating frontend

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password != confirmPassword) {
+      alert("password does not match, sahi se password dal chutiye");
+      return;
+    }
+
+    try {
+      // react query
+      // loading, dupicacy, error, debouncing
+
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || "Registration failed");
+      }
+
+      console.log(data);
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>RegisterPage</div>
-  )
+    <div>
+      <h1>Register Page</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter the Email ID"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter the password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <div>
+        <p>
+          already have an account? <a href="/login">Login</a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
