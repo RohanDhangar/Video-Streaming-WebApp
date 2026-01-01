51669a468f42f7c22d940447c267fca65744a938
Rohan Dhangar <rohandhangar1109@gmail.com>
2026-01-01 20:47:02 +0530

    added code in signIn

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,51 @@
 "use client";
-import React from 'react'
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      console.log(result.error);
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
-    <div>LoginPage</div>
-  )
+    <div>
+      <h1>login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter the Email ID"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter the password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit"> Submit </button>
+      </form>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
