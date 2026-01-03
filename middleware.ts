5bbdac6e83c872f94ce5629fc88b0babad4dec18
Rohan Dhangar <rohandhangar1109@gmail.com>
2026-01-03 21:00:07 +0530

    middleware completed

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,41 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-export default withAuth({
-  // Matches the pages config in `[...nextauth]`
-  pages: {
-    signIn: "/login",
-    error: "/error",
+export default withAuth(
+  function middleware() {
+    return NextResponse.next();
   },
-})
+  {
+    callbacks: {
+      authorized({ token, req }) {
+        const { pathname } = req.nextUrl;
+        if
+          (
+          pathname.startsWith("/api/auth") ||
+          pathname === "/login" ||
+          pathname === "/register"
+        ) {
+          return true
+        }
+
+        if
+          (
+          pathname === "/" ||
+          pathname.startsWith("/api/video")
+        ) {
+          return true;
+        }
+
+        return !!token;
+      },
+    },
+  }
+);
 
 
 // this config basically defines ki kin routes pr middleware ko chalna hai, we can define manually also all the required routes 
 export const config = {
-  matcher : [
+  matcher: [
     // this below expression tells that run the middleware on all the expect the below one - it is basically default snippt for middleware
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
